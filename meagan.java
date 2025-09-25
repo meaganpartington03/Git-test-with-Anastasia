deb7eff67670186ce343b7c55cec6f83ae57d526
Anastasia <annie.sadovski@gmail.com>
2025-09-25 10:54:20 -0400

    Anastasia's edit in Meagan's Java file

--- meagan.java
+++ meagan.java
@@ -1,6 +1,7 @@
 public class meagan {
     public static void main(String[] args) {
     System.out.println("Hello Anastasia!") ;
+    System.out.print("Anastasia's first edit in Meagan's document");
     }
 }
 
